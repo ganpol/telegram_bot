1ead748fda07ce337dd5e5372128df71a67058a5
ganpol <ganpol@gmail.com>
2021-11-26 22:44:54 +0800

    fist bot

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+
+TOKEN = '2049636346:AAEjEJTXrXhJU3zLspBnIF42qV8ezRYajmk'
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'Рубль': 'RUB',
+    'Доллар': 'USD',
+    'Евро': 'EURO',
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введине команду боту в следующем формате:\n' \
+           '<имя валюты> <в какую валюты перевести> <кол-во переводимой валюты>\
+           Увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+        bot.reply_to(message, text)
+
+
+bot.polling()
