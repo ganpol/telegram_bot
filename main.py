1bdbd131a41ef4de3567aa50dabd85e3b95bc4ae
ganpol <ganpol@gmail.com>
2021-11-27 01:04:40 +0800

    4

--- main.py
+++ main.py
@@ -22,15 +22,21 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(' ')
+    try:
+        values = message.text.split(' ')
 
-    if len != 3:
-        raise ConvertionException('Слишком много параметров.')
+        if len != 3:
+            raise ConvertionException('Слишком много параметров.')
 
-    quote, base, amount = values
+        quote, base, amount = values
+        total_base = CurrencyConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователся\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
 
-    total_base = CurrencyConverter.convert(quote, base, amount)
-    text = f'Цена {amount} {quote} в {base} - {total_base}'
-    bot.send_message(message.chat.id, text)
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
 
 bot.polling()
